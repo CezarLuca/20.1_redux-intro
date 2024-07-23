60f3064dde7ed78c65a2f7903fd02c5aa75b648f
Cezar Luca <cezar.luca@hotmail.com>
2024-07-23 21:59:57 +0200

    Professional Redux FIle Structure State Slices part 2

--- src/store-v1.js
+++ src/store-v1.js
@@ -1,10 +1,10 @@
 import { combineReducers, createStore } from "redux";
 
-// const initialState = {
-//     balance: 0,
-//     loan: 0,
-//     loanPurpose: "",
-// };
+const initialState = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: "",
+};
 
 const initialStateCustomer = {
     fullName: "",
@@ -12,37 +12,37 @@ const initialStateCustomer = {
     createdAt: "",
 };
 
-// function accountReducer(state = initialState, action) {
-//     switch (action.type) {
-//         case "account/deposit":
-//             return {
-//                 ...state,
-//                 balance: state.balance + action.payload,
-//             };
-//         case "account/withdraw":
-//             return {
-//                 ...state,
-//                 balance: state.balance - action.payload,
-//             };
-//         case "account/requestLoan":
-//             return {
-//                 // LATER
-//                 ...state,
-//                 loan: action.payload.amount,
-//                 loanPurpose: action.payload.purpose,
-//                 balance: state.balance + action.payload.amount,
-//             };
-//         case "account/payLoan":
-//             return {
-//                 ...state,
-//                 loan: 0,
-//                 loanPurpose: "",
-//                 balance: state.balance - state.loan,
-//             };
-//         default:
-//             return state;
-//     }
-// }
+function accountReducer(state = initialState, action) {
+    switch (action.type) {
+        case "account/deposit":
+            return {
+                ...state,
+                balance: state.balance + action.payload,
+            };
+        case "account/withdraw":
+            return {
+                ...state,
+                balance: state.balance - action.payload,
+            };
+        case "account/requestLoan":
+            return {
+                // LATER
+                ...state,
+                loan: action.payload.amount,
+                loanPurpose: action.payload.purpose,
+                balance: state.balance + action.payload.amount,
+            };
+        case "account/payLoan":
+            return {
+                ...state,
+                loan: 0,
+                loanPurpose: "",
+                balance: state.balance - state.loan,
+            };
+        default:
+            return state;
+    }
+}
 
 function customerReducer(state = initialStateCustomer, action) {
     switch (action.type) {
@@ -87,21 +87,21 @@ const store = createStore(rootReducer);
 // store.dispatch({ type: "account/payLoan" });
 // console.log(store.getState());
 
-// function deposit(amount) {
-//     return { type: "account/deposit", payload: amount };
-// }
+function deposit(amount) {
+    return { type: "account/deposit", payload: amount };
+}
 
-// function withdraw(amount) {
-//     return { type: "account/withdraw", payload: amount };
-// }
+function withdraw(amount) {
+    return { type: "account/withdraw", payload: amount };
+}
 
-// function requestLoan(amount, purpose) {
-//     return { type: "account/requestLoan", payload: { amount, purpose } };
-// }
+function requestLoan(amount, purpose) {
+    return { type: "account/requestLoan", payload: { amount, purpose } };
+}
 
-// function payLoan() {
-//     return { type: "account/payLoan" };
-// }
+function payLoan() {
+    return { type: "account/payLoan" };
+}
 
 store.dispatch(deposit(500));
 console.log(store.getState());
